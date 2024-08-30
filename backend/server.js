06149adfedbb92ff48ffb5ef38a1714d84ce24fb
Tenlive1 <nguyenperry99@gmail.com>
2024-08-30 18:26:43 -0500

    started on the back end where we install express, nodemon and used a bit of middleware and did some routes

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+
+//express app
+const app = express();
+
+//middleware
+app.use((req,res,next)=>{
+    console.log(req.path,req.method);
+    next();
+})
+
+//routes
+app.get('/',(req,res) =>{
+    res.json({message: "welcome to the app"});
+})
+
+//listen for request
+app.listen(process.env.PORT, ()=>{
+    console.log(`listen to ` + process.env.PORT );
+})
+
+process.env
