de2437382c0991189fb2193b5d6b4fb0e72e0e57
Tenlive1 <nguyenperry99@gmail.com>
2024-09-02 19:03:20 -0500

    done with full stack

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const workoutRoutes = require('./routes/workouts')
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 //express app
 const app = express();
 
@@ -12,7 +12,7 @@ app.use((req,res,next)=>{
     console.log(req.path,req.method);
     next();
 })
-
+app.use(cors());
 //routes    
 app.use('/api/workouts',workoutRoutes);
 
